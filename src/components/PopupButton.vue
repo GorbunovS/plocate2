d7dbee83d946091b70ed6e72727fbbcfe223d45b
GorbunovS <obes1stas1@gmail.com>
2025-09-09 16:13:21 +0300

    set proj

---
+++ src/components/PopupButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+/**
+ * Component which opens native Telegram Popup.
+ */
+import { popup } from '@telegram-apps/sdk-vue'
+
+const props = defineProps<{ title: string, message: string }>()
+
+function open() {
+  if (popup.isSupported()) {
+    popup.open(props);
+    return;
+  }
+
+  // Open fallback HTML dialog...
+}
+</script>
+
+<template>
+  <button aria-haspopup="dialog" @click="open">
+    Open popup
+  </button>
+</template>
