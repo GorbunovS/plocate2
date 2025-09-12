ca148d5bf3cba3cc0b57f366f65363bc1e1e9cdf
GorbunovS <obes1stas1@gmail.com>
2025-09-12 17:06:31 +0300

    ка

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),  tailwindcss(),],
-})
+esbuild: {
+    // Игнорировать определённые предупреждения или ошибки (если нужно)
+    legalComments: 'none',
+    // Или отключить минификацию для теста
+    minify: false,
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true, // Это помогает с mixed ES/CJS модулями[5]
+    },
+  },
+});
