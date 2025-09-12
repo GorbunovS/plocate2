3789be5175f3f1b68d542b490eaa121c256e8358
GorbunovS <obes1stas1@gmail.com>
2025-09-12 17:13:01 +0300

    ed

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,31 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import terser from 'vite-plugin-terser';
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    terser(),  // Плагин для Terser
+  ],
   esbuild: {
+    // Отключить esbuild для JS-трансформации
+    drop: ['console', 'debugger'],  // Удалить console для теста
     legalComments: 'none',
   },
   build: {
-    minify: 'terser',  // Переход на Terser вместо esbuild
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,  // Удалить console, чтобы уменьшить код
+      },
+      mangle: true,
+    },
     commonjsOptions: {
       transformMixedEsModules: true,
     },
-    terserOptions: {
-      compress: true,  // Включите сжатие, если нужно
-      mangle: true,    // Минификация имён
-    },
+    // Игнорировать дубликаты в chunk'ах
+    chunkSizeWarningLimit: 1000,
   },
 });
