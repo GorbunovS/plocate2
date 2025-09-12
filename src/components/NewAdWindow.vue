d21d781da21fc3c93ed567ff37f72823d475d2e1
GorbunovS <obes1stas1@gmail.com>
2025-09-12 11:32:26 +0300

    pipa

--- src/components/NewAdWindow.vue
+++ src/components/NewAdWindow.vue
@@ -3,9 +3,9 @@
     <div class="flex flex-col items-start gap-4 p-4">
         Описание
         <span class="text-sm text-gray-500 italic">Тип объявления</span>
-            <SelectButton :invalid="value === null" v-model="adType" :options="adTypes" optionLabel="name" />
+            <SelectButton :invalid="adType === null" v-model="adType" :options="adTypes" optionLabel="name" />
           <span class="text-sm text-gray-500 italic">Тип животного</span>  
-            <SelectButton :invalid="value === null"  v-model="petType" :options="petTypes" optionLabel="name" />
+            <SelectButton :invalid="petType === null"  v-model="petType" :options="petTypes" optionLabel="name" />
                     <SelectButton v-model="value" :options="options" optionLabel="value" dataKey="value" aria-labelledby="custom">
             <template #option="slotProps">
                 <i :class="slotProps.option.icon"></i>
