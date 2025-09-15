1fcf67dbbe3e8a366d7d9fd4cebcc5e7101581b1
GorbunovS <obes1stas1@gmail.com>
2025-09-15 14:22:40 +0300

    kekus

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import AutoComplete from 'primevue/autocomplete';
 import { VueTelegramPlugin } from 'vue-tg';
 import { init } from '@telegram-apps/sdk';
 import { locationManager } from '@telegram-apps/sdk';
-
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
 
 
 init();
